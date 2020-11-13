901742b6d029020e3e252959833d3ab352b41d32
Stephen Axtell <axtell.stephen@gmail.com>
2020-11-13 16:43:08 +0200

    created form and state to create people

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import SetUp from './tutorial/5-useRef/1-useRef-basics';
+import SetUp from './tutorial/6-useReducer/';
 
 function App() {
   
--- react-advanced-2020/src/tutorial/6-useReducer/index.js
+++ react-advanced-2020/src/tutorial/6-useReducer/index.js
@@ -1,10 +1,46 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
-import { data } from '../../../data';
+import { data } from '../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  // state
+  const [name, setName] = useState('');
+  const [people, setPeople] = useState(data);
+  const [showModal, setModal] = useState( );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (name) {
+      setModal(true);
+      setPeople([...people, { id: new Date().getTime().toString(), name}]);
+      setName('');
+    } else {
+      setModal(true);
+    }    
+  }
+
+  return (
+    <>
+      { showModal && <Modal />}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <button type="submit" title="Add Person">Add Person</button>
+      </form>
+      {
+        people.map(person => {
+          return (
+            <div key={person.id}>
+              <h4>{person.name}</h4>
+            </div> 
+          )
+        })
+      }
+    </>
+  );
 };
 
 export default Index;
