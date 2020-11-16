e69f832582971d92878b3228aba3ad3cbe656158
Stephen Axtell <axtell.stephen@gmail.com>
2020-11-16 10:25:25 +0200

    refactor using useReducer to manage state, we allowed adding item as well as removing item

--- react-advanced-2020/src/tutorial/6-useReducer/index.js
+++ react-advanced-2020/src/tutorial/6-useReducer/index.js
@@ -1,29 +1,76 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../data';
+
 // reducer function
+const reducer = (state, action) => {
+
+  // can put this is a separate file pluse use the case structure
+  if (action.type === 'ADD_ITEM') {
+    const newPeople = [...state.people, action.payload];
+    // you have to return state at least
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: 'Item added'
+    };
+  }
+
+  if (action.type === 'NO_VALUE') {
+    return {
+      ...state,      
+      isModalOpen: true,
+      modalContent: 'Please enter value'
+    };
+  }
+
+  if (action.type === 'CLOSE_MODAL') {
+    return {
+      ...state,
+      isModalOpen: false
+    }
+  }
+
+  if (action.type === 'REMOVE_ITEM') {
+    const newPeople = state.people.filter((person) => person.id !== action.payload);
+    return {...state, people: newPeople};
+  }
+
+  throw new Error('No matching action type'); 
+};
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: 'hi there'
+};
 
 const Index = () => {
   // state
   const [name, setName] = useState('');
-  const [people, setPeople] = useState(data);
-  const [showModal, setModal] = useState( );
+  
+  // reducer
+  // Just keep in mind to think in part of the way we did things with Redux, dispatching the action and doing something with the state in the reducer based on the action type
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (name) {
-      setModal(true);
-      setPeople([...people, { id: new Date().getTime().toString(), name}]);
+      const newItem = { id: new Date().getTime().toString(), name};
+      dispatch({ type: 'ADD_ITEM', payload: newItem});
       setName('');
     } else {
-      setModal(true);
+      dispatch({ type: 'NO_VALUE'});
     }    
   }
 
+  const closeModal = () => dispatch({type: 'CLOSE_MODAL'});
+
   return (
     <>
-      { showModal && <Modal />}
+      { state.isModalOpen && <Modal modalContent={state.modalContent} closeModal={closeModal} />}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
@@ -31,10 +78,11 @@ const Index = () => {
         <button type="submit" title="Add Person">Add Person</button>
       </form>
       {
-        people.map(person => {
+        state.people.map(person => {
           return (
-            <div key={person.id}>
+            <div key={person.id} className="item">
               <h4>{person.name}</h4>
+              <button onClick={() => dispatch({ type: 'REMOVE_ITEM', payload: person.id})} title="Remove">Remove</button>
             </div> 
           )
         })
